341a2d34494342003f021b257e1fbe4cadefe515
CEMOMRT <142478485+CEMOMRT@users.noreply.github.com>
2023-09-29 14:40:41 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace RecapProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxCategory = new System.Windows.Forms.GroupBox();
+            this.gbxProductName = new System.Windows.Forms.GroupBox();
+            this.dgwProducts = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxSearch = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxCategory = new System.Windows.Forms.ComboBox();
+            this.gbxCategory.SuspendLayout();
+            this.gbxProductName.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbxCategory
+            // 
+            this.gbxCategory.Controls.Add(this.cbxCategory);
+            this.gbxCategory.Controls.Add(this.label1);
+            this.gbxCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gbxCategory.Location = new System.Drawing.Point(18, 18);
+            this.gbxCategory.Name = "gbxCategory";
+            this.gbxCategory.Size = new System.Drawing.Size(941, 131);
+            this.gbxCategory.TabIndex = 0;
+            this.gbxCategory.TabStop = false;
+            this.gbxCategory.Text = "List by Category";
+            // 
+            // gbxProductName
+            // 
+            this.gbxProductName.Controls.Add(this.label2);
+            this.gbxProductName.Controls.Add(this.tbxSearch);
+            this.gbxProductName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gbxProductName.Location = new System.Drawing.Point(18, 195);
+            this.gbxProductName.Name = "gbxProductName";
+            this.gbxProductName.Size = new System.Drawing.Size(939, 123);
+            this.gbxProductName.TabIndex = 1;
+            this.gbxProductName.TabStop = false;
+            this.gbxProductName.Text = "List By Product";
+            // 
+            // dgwProducts
+            // 
+            this.dgwProducts.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgwProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwProducts.Location = new System.Drawing.Point(18, 356);
+            this.dgwProducts.Name = "dgwProducts";
+            this.dgwProducts.RowHeadersWidth = 51;
+            this.dgwProducts.RowTemplate.Height = 24;
+            this.dgwProducts.Size = new System.Drawing.Size(941, 227);
+            this.dgwProducts.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(47, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Category";
+            // 
+            // tbxSearch
+            // 
+            this.tbxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbxSearch.Location = new System.Drawing.Point(186, 64);
+            this.tbxSearch.Name = "tbxSearch";
+            this.tbxSearch.Size = new System.Drawing.Size(384, 27);
+            this.tbxSearch.TabIndex = 1;
+            this.tbxSearch.TextChanged += new System.EventHandler(this.tbxSearch_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(47, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Product";
+            // 
+            // cbxCategory
+            // 
+            this.cbxCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cbxCategory.FormattingEnabled = true;
+            this.cbxCategory.Location = new System.Drawing.Point(186, 58);
+            this.cbxCategory.Name = "cbxCategory";
+            this.cbxCategory.Size = new System.Drawing.Size(384, 28);
+            this.cbxCategory.TabIndex = 1;
+            this.cbxCategory.SelectedIndexChanged += new System.EventHandler(this.cbxCategory_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ClientSize = new System.Drawing.Size(992, 649);
+            this.Controls.Add(this.dgwProducts);
+            this.Controls.Add(this.gbxProductName);
+            this.Controls.Add(this.gbxCategory);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Recap Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbxCategory.ResumeLayout(false);
+            this.gbxCategory.PerformLayout();
+            this.gbxProductName.ResumeLayout(false);
+            this.gbxProductName.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProducts)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxCategory;
+        private System.Windows.Forms.GroupBox gbxProductName;
+        private System.Windows.Forms.DataGridView dgwProducts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxSearch;
+        private System.Windows.Forms.ComboBox cbxCategory;
+    }
+}
+
